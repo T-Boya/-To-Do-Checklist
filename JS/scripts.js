83a9eb2af3eae2af6e9bf26001bcc1ada2901a15
T-Boya <t.osewe1@gmail.com>
2018-02-27 16:30:14 +0300

    created files, attempted to create checklist twice. First attempted was deleted/comented out. Seccond attempt is in progress. Little progress has been made.

---
+++ JS/scripts.js
@@ -0,0 +1,31 @@
+// function Task(item) {
+//     this.item = item;
+// }
+
+// user interface logic
+// $(document).ready(function() {
+//     $("form#paper").submit(function(event) {
+//       event.preventDefault();
+
+// var inputtednewitem = $("input.").val()
+
+
+//       var inputtedFirstName = $("input#new-first-name").val();
+
+//       var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+//       $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+//       $("input#new-first-name").val("");
+//       $("input#new-last-name").val("");
+//     });
+//   });
+  
+// clone attempt - do not use
+// setInterval(function(){
+//     if($('#newitem').html()!=$('#newbox').html()){
+//          $('#newitem').html($('#newbox').html());
+//     }
+//  },4000);
+
+
